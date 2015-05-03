fbc94ad81728da46099486fe4d93810383fa86b7
xnie <niexin.study@gmail.com>
2015-05-03 16:56:03 +0200

    number to money formatter

---
+++ numberToMoney.js
@@ -0,0 +1,19 @@
+// TODO: so ugly, needs refacto
+// http://www.codewars.com/kata/54ad586282bc65b7ec000182/train/javascript
+var numberToMoney = function(n) {
+  val = n.toString().split('.');
+  
+  var decimalPart = val[1].substr(0, 2);
+  if ( (+decimalPart)%10 === 0 ) {
+    decimalPart = decimalPart.substr(0, decimalPart.length-1);
+  }
+  
+  var integerPart = val[0].split('');
+  for (var i = integerPart.length -3; i > 0; i -= 3) {
+    integerPart.splice(i, 0, ',');
+  }
+  integerPart = integerPart.join(''); 
+  
+  
+  return integerPart + '.' + decimalPart;
+};
