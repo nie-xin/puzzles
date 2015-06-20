495b755ea1c7500006a86e32f17cd8b28005cdba
nie xin <niexin.study@gmail.com>
2015-06-20 21:47:59 +0200

    table layout

---
+++ tableLayout.js
@@ -0,0 +1,111 @@
+// rows: hold an array of arrays, each inner array representing a row of cells
+function rowHeights(rows) {
+  return rows.map(function(row) {
+    return row.reduce(function(max, cell) {
+      return Math.max(max, cell.minHeight());
+    }, 0);
+  });
+}
+
+function colWidths(rows) {
+  return rows[0].map(function(_, i) {
+    return rows.reduce(function(max, row) {
+      return Math.max(max, row[i].minWidth());
+    }, 0);
+  });
+}
+
+function drawTable(rows) {
+  var heights = rowHeights(rows);   // contains heights of each row
+  var widths = colWidths(rows);     // contains widths of each column
+
+  function drawLine(blocks, lineNo) {
+    return blocks.map(function(block) {
+      return block[lineNo];
+    }).join(" ");
+  }
+
+  function drawRow(row, rowNum) {
+    var blocks = row.map(function(cell, colNum) {
+      return cell.draw(widths[colNum], heights[rowNum]);
+    });
+    return blocks[0].map(function(_, lineNo) {
+      return drawLine(blocks, lineNo);
+    }).join("\n");
+  }
+
+  return rows.map(drawRow).join("\n");
+}
+
+function repeat(string, times) {
+  var result = "";
+  for (var i = 0; i < times; i++)
+    result += string;
+  return result;
+}
+
+function TextCell(text) {
+  this.text = text.split("\n");
+}
+TextCell.prototype.minWidth = function() {
+  return this.text.reduce(function(width, line) {
+    return Math.max(width, line.length);
+  }, 0);
+};
+TextCell.prototype.minHeight = function() {
+  return this.text.length;
+}
+TextCell.prototype.draw = function(width, height) {
+  var result = [];
+  for (var i = 0; i < height; i++) {
+    var line = this.text[i] || "";
+    result.push(line + repeat(" ", width - line.length));
+  }
+  return result;
+};
+
+function UnderLineCell(inner) {
+  this.inner = inner;
+}
+UnderLineCell.prototype.minWidth = function() {
+  return this.inner.minWidth();
+};
+UnderLineCell.prototype.minHeight = function() {
+  return this.inner.minHeight() + 1;
+};
+UnderLineCell.prototype.draw = function(width, height) {
+  return this.inner.draw(width, height - 1)
+    .concat([repeat("-", width)]);
+};
+
+function dataTable(data) {
+  var keys = Object.keys(data[0]);
+  var headers = keys.map(function(name) {
+    return new UnderLineCell(new TextCell(name));
+  });
+  var body = data.map(function(row) {
+    return keys.map(function(name) {
+      var value = row[name];
+      if (typeof value == 'number')
+        return new RTextCell(String(value));
+      else
+        return new TextCell(String(row[name]));
+    });
+  });
+
+  return [headers].concat(body);
+}
+
+function RTextCell(text) {
+  TextCell.call(this, text);
+}
+RTextCell.prototype = Object.create(TextCell.prototype); // inheritance
+RTextCell.prototype.draw = function(width, height) {
+  var result = [];
+  for (var i = 0; i < height; i++) {
+    var line = this.text[i] || "";
+    result.push(repeat(" ", width - line.length) + line); // white spaces first
+  }
+
+  return result;
+};
---
+++ vector.js
@@ -0,0 +1,13 @@
+function Vector(x, y) {
+  this.x = x;
+  this.y = y;
+}
+Vector.prototype.plus = function(v) {
+  return new Vector(this.x + v.x, this.y + v.y);
+};
+Vector.prototype.minus = function(v) {
+  return new Vector(this.x - v.x, this.y - v.y);
+};
+Object.defineProperty(Vector.prototype, 'length', {
+  get: function() { return Math.sqrt(this.x * this.x + this.y * this.y); }
+});
