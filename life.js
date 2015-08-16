28220056f428cd96fade7daaa0e8714c67b93823
nie xin <niexin.study@gmail.com>
2015-08-16 11:43:30 +0200

    life simulator

---
+++ life.js
@@ -0,0 +1,92 @@
+/**
+ * plan presents the world
+ * # - walls & rocks
+ * o - criters
+ */
+var plan = ["############################",
+            "#      #    #      o      ##",
+            "#                          #",
+            "#          #####           #",
+            "##         #   #    ##     #",
+            "###           ##     #     #",
+            "#           ###      #     #",
+            "#   ####                   #",
+            "#   ##       o             #",
+            "# o  #         o       ### #",
+            "#    #                     #",
+            "############################"];
+
+
+/**
+ * The coordinate of squares inside the world
+ * @param {int} x x-coordinate
+ * @param {int} y y-coordinate
+ */
+function Vector(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+Vector.prototype.plus = function(other) {
+  return new Vector(this.x + other.x, this.y + other.y);
+}
+
+/**
+ * The grid that models the world
+ * @param {int} width
+ * @param {int} height
+ */
+function Grid(width, height) {
+  this.space = new Array(width * height);
+  this.width = width;
+  this.height = height;
+}
+
+Grid.prototype.isInside = function(vector) {
+  return vector.x >= 0 && vector.x < this.width &&
+         vector.y >= 0 && vector.y < this.height;
+};
+
+Grid.prototype.get = function(vector) {
+  return this.space[vector.x + this.width * vector.y];
+};
+
+Grid.prototype.set = function(vector, value) {
+  this.space[vector.x + this.width * vector.y] = value;
+};
+
+
+var directions = {
+  "n":  new Vector( 0, -1),
+  "ne": new Vector( 1, -1),
+  "e":  new Vector( 1,  0),
+  "se": new Vector( 1,  1),
+  "s":  new Vector( 0,  1),
+  "sw": new Vector(-1,  1),
+  "w":  new Vector(-1,  0),
+  "nw": new Vector(-1, -1)
+};
+
+var directionNames = "n ne e se s sw w nw".split(" ");
+
+function randomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function BouncingCritter() {
+  this.direction = randomElement(directionNames);
+}
+/**
+ * The action taking for each round
+ * @param  {object} view the surroundings of a critter. It has a look method
+ * that takes a direction and returns a character. It has also a find & findAll
+ * method that return direction of a character
+ * @return {object}      type of the action & extra info (e.g. direction to move)
+ */
+BouncingCritter.prototype.act = function(view) {
+  if (view.look(this.direction) != " ")
+    this.direction = view.find(" ") || "s";
+
+  return {type: "move", direction: this.direction};
+};
+
